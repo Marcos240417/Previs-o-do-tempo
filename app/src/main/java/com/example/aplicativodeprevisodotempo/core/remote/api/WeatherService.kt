7849197ccaa7fac932c158fa7c0eff4b8c2717d0
Estudo Kotlin Android <mr6227017@gmail.com>
2026-03-22 15:24:28 -0300

    feat: define os endpoints do Retrofit para chamadas à API OpenWeather

---
+++ app/src/main/java/com/example/aplicativodeprevisodotempo/core/remote/api/WeatherService.kt
@@ -0,0 +1,27 @@
+package com.example.aplicativodeprevisodotempo.core.remote.api
+
+import com.example.aplicativodeprevisodotempo.core.data.utils.Constants // Importe suas constantes
+import com.example.aplicativodeprevisodotempo.core.remote.dto.WeatherResponse
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface WeatherService {
+
+    @GET("weather")
+    suspend fun getWeather(
+        @Query("lat") latitude: String,
+        @Query("lon") longitude: String,
+        @Query("appid") appid: String = Constants.API_KEY,
+        @Query("units") units: String = "metric",
+        @Query("lang") lang: String = "pt_br"
+    ): Response<WeatherResponse>
+
+    @GET("weather")
+    suspend fun getWeatherByCityName(
+        @Query("q") cityName: String,
+        @Query("appid") appid: String = Constants.API_KEY,
+        @Query("units") units: String = "metric",
+        @Query("lang") lang: String = "pt_br"
+    ): Response<WeatherResponse>
+}
