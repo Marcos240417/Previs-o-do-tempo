254a096d380a2df3e9f7e4899a5b1de86620b0ca
Estudo Kotlin Android <mr6227017@gmail.com>
2026-03-22 15:20:11 -0300

    docs: mapeia dados de coordenadas geográficas da resposta da API

---
+++ app/src/main/java/com/example/aplicativodeprevisodotempo/core/remote/dto/CoordResponse.kt
@@ -0,0 +1,9 @@
+package com.example.aplicativodeprevisodotempo.core.remote.dto
+
+import com.google.gson.annotations.SerializedName
+
+data class CoordResponse(
+    @SerializedName("lat") val lat: Double,
+    @SerializedName("lon") val lon: Double
+
+)
