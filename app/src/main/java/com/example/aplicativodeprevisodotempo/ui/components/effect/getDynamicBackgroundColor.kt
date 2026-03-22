635e4f44c1cce08217da152569b0ce9149c38a64
Estudo Kotlin Android <mr6227017@gmail.com>
2026-03-22 16:31:00 -0300

    chore: retirada de comentários

--- app/src/main/java/com/example/aplicativodeprevisodotempo/ui/components/effect/getDynamicBackgroundColor.kt
+++ app/src/main/java/com/example/aplicativodeprevisodotempo/ui/components/effect/getDynamicBackgroundColor.kt
@@ -2,17 +2,22 @@ package com.example.aplicativodeprevisodotempo.ui.components.effect
 
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
+import androidx.compose.ui.graphics.Brush
 import com.example.aplicativodeprevisodotempo.ui.theme.DayGradient
 import com.example.aplicativodeprevisodotempo.ui.theme.NightGradient
 import java.util.Calendar
 
 @Composable
-fun getDynamicBackgroundColor(): androidx.compose.ui.graphics.Brush {
+fun getDynamicBackgroundColor(f: Float): Brush {
 
     val hour = remember { Calendar.getInstance().get(Calendar.HOUR_OF_DAY) }
-
-
     val isNight = hour !in 6..17
 
-    return if (isNight) NightGradient else DayGradient
+    return remember(f, isNight) {
+        if (isNight) {
+            NightGradient
+        } else {
+            DayGradient
+        }
+    }
 }
