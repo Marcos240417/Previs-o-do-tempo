8ff466227d8d7d00d480e5ed899719fb05d732f3
Estudo Kotlin Android <mr6227017@gmail.com>
2026-03-22 15:19:51 -0300

    docs: define modelo de resposta para detalhes meteorológicos

---
+++ app/src/main/java/com/example/aplicativodeprevisodotempo/core/remote/dto/WeatherDetailResponse.kt
@@ -0,0 +1,8 @@
+package com.example.aplicativodeprevisodotempo.core.remote.dto
+
+import com.google.gson.annotations.SerializedName
+
+data class WeatherDetailResponse(
+    @SerializedName("description") val description: String,
+    @SerializedName("icon") val icon: String
+)
