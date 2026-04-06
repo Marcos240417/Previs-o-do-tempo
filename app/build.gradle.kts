03aad52d53d6ce84da64ee3ed647992c3ebbbc62
Estudo Kotlin Android <mr6227017@gmail.com>
2026-04-06 10:44:27 -0300

    refactor: reestruturação com upgrade para JavaVersion.VERSION_21 solicitado a partir do SDK 36.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,4 +1,3 @@
-import java.io.FileInputStream
 import java.util.Properties
 
 plugins {
@@ -42,8 +41,8 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
+        sourceCompatibility = JavaVersion.VERSION_21
+        targetCompatibility = JavaVersion.VERSION_21
     }
     buildFeatures {
         compose = true
